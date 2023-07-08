fdc8b9430548c93306ce32a20e93d95b828f42e4
imam-hossain <zunayedemon6t9@gmail.com>
2023-07-08 21:08:56 +0600

    admin model done

---
+++ src/app/modules/admin/admin.model.ts
@@ -0,0 +1,49 @@
+import { Schema, model } from "mongoose";
+import { IAdmin } from "./admin.interface";
+
+
+const AdminSchema = new Schema<IAdmin>({
+    _id: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['admin'],
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+
+  export const Admin = model<IAdmin>('Admin', AdminSchema);
