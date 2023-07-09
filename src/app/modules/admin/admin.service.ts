1164efb3ad9f679ddf9b0dfd94704ef115f224e0
imam-hossain <zunayedemon6t9@gmail.com>
2023-07-09 20:05:15 +0600

    create access token service file done

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -2,14 +2,17 @@ import httpStatus from 'http-status';
 import ApiError from '../../../errors/ApiError';
 import { IAdmin, ILoginUser, ILoginUserResponse } from './admin.interface';
 import { Admin } from './admin.model';
+import { jwtHelpers } from '../../../helpers/jwtHelpers';
+import config from '../../../config';
+import { Secret } from 'jsonwebtoken';
 
 const createAdmin = async (payload: IAdmin):  Promise<IAdmin> => {
   const admin = await Admin.create(payload);
   return admin;
 };
-//: Promise<ILoginUserResponse>
 
-const logInAdmin = async (payload:ILoginUser) => {
+
+const logInAdmin = async (payload:ILoginUser): Promise<ILoginUserResponse> => {
   const { id, password } = payload;
   const isUserExist = await Admin.isUserExist(id);
 
@@ -23,9 +26,14 @@ const logInAdmin = async (payload:ILoginUser) => {
     throw new ApiError(httpStatus.UNAUTHORIZED, 'Password is incorrect');
   }
 
+  const { _id: userId, role, needsPasswordChange } = isUserExist;
+  
+  const accessToken = jwtHelpers.createToken(
+    { userId, role },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  );
 
-  const admin = await Admin.create(payload);
-  return admin;
 };
 
 
